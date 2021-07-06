bd8e47e75ee0a6ebceb7c4134ec41d9097d2ca3c
SeaChan <1341914555@qq.com>
2021-07-06 16:11:03 +0800

    init

---
+++ config/database.js
@@ -0,0 +1,33 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'mysql',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'blog'),
+        username: env('DATABASE_USERNAME', 'seachan'),
+        password: env('DATABASE_PASSWORD', 'cs_551507'),
+      },
+      options: {},
+    },
+  },
+});
+
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'sqlite',
+//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+//       },
+//       options: {
+//         useNullAsDefault: true,
+//       },
+//     },
+//   },
+// });
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '35114a75-dc6c-4be4-b7b0-9e0009081944'
+};
