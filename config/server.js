20b3826eeb080645da09161997ddd8443f17267d
SeaChan <1341914555@qq.com>
2021-07-06 21:18:30 +0800

    port change to 1337

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 3001),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'd8f681024b155dbcd0a92fe2bf396b08'),
